7f957e1e4be1e367d265fd7f3188815e5fd0d789
Pavan <kolipaka.pavan.kp@gmail.com>
2024-05-31 21:03:35 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -5,17 +5,13 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-// Set EJS as the template engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
 app.get("/", (req, res) => {
   res.render("index", { qrCodeSrc: null, error: null });
 });
@@ -40,7 +36,6 @@ app.post("/generate", (req, res) => {
   });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
